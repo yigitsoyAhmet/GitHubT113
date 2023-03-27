64cd5509aa39194e3fcacf67f7436bd08646bc83
AhmetYigitsoy <ahmetyigitsoy92@gmail.com>
2023-03-27 23:50:21 +0300

    230327

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -1,6 +1,9 @@
 public class gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("==========");
+        System.out.println("Have a wice day");
+
     }
 }
 
