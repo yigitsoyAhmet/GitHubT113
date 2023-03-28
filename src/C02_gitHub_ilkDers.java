d8239239d07bc25de335d41dba1ce09e6a41c0b7
AhmetYigitsoy <ahmetyigitsoy92@gmail.com>
2023-03-28 21:51:50 +0300

    ikinciGun2

--- src/C02_gitHub_ilkDers.java
+++ src/C02_gitHub_ilkDers.java
@@ -1,2 +1,11 @@
 public class C02_gitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("Merhaba gitHub ben geldim");
+        System.out.println("=======");
+        System.out.println("Have a wise day");
+        System.out.println("gitadd-commit-push");
+        System.out.println("Merhaba ben wise");
+
+
+    }
 }
